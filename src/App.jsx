b3bedee23db65bd8b747024c2469bb480577d4bf
hadhirahman <hadhirahman@gmail.com>
2025-11-02 20:04:32 +0530

    react routes setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 
-
+import { Route, Routes,BrowserRouter } from "react-router-dom"
+import Body from "./components/Body"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import Profile from "./components/Profile"
 function App() {
 
   return (
     <>
-      
-      <h1>hello world</h1>
-    
-    </>
+    <BrowserRouter basename="/">     
+    <Routes>
+      <Route path="/" element={<Body/>}>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/dashboard" element={<div>Dashboard Page</div>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="*" element={<div>404 Not Found</div>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </> 
   )
 }
 
